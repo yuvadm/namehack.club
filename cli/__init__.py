22609ad8dd38416faee673c229c738c0ad645940
Yuval Adam <_@yuv.al>
2023-05-02 10:27:54 +0300

    Migrate ghpr script

--- cli/__init__.py
+++ cli/__init__.py
@@ -2,4 +2,5 @@ from .base import cli
 
 from .build import build
 from .clean import clean
+from .ghpr import ghpr
 from .scan import scan
--- cli/ghpr.py
+++ cli/ghpr.py
@@ -1,35 +1,40 @@
+import click
 import sys
 
 from pathlib import Path
 from subprocess import run
 
+from .base import cli
+
 ROOT_PATH = Path(__file__).parents[1]
 
-path = sys.argv[1]
 
-with open(path, "r") as f:
-    for line in f:
-        l = line.strip().split(",")
-        domain, handle, name = [x.strip() for x in l]
-        handle = None if "@" in handle else handle
+@cli.command()
+@click.argument("path", type=click.Path())
+def ghpr(path):
+    with open(path, "r") as f:
+        for line in f:
+            l = line.strip().split(",")
+            domain, handle, name = [x.strip() for x in l]
+            handle = None if "@" in handle else handle
 
-        fn = domain.replace(".", "")
+            fn = domain.replace(".", "")
 
-        path = ROOT_PATH / "names" / f"{fn}.yml"
-        if path.exists():
-            print(f"{domain} already exists")
-            continue
+            path = ROOT_PATH / "names" / f"{fn}.yml"
+            if path.exists():
+                print(f"{domain} already exists")
+                continue
 
-        with open(path, "w") as out:
-            s = f"domain: {domain}\nname: {name}\n"
-            if handle:
-                s += f"github: {handle}\n"
-            s += "candidate: true\n"
-            out.write(s)
+            with open(path, "w") as out:
+                s = f"domain: {domain}\nname: {name}\n"
+                if handle:
+                    s += f"github: {handle}\n"
+                s += "candidate: true\n"
+                out.write(s)
 
-        # run(["git", "checkout", "-b", domain])
-        # run(["git", "add", f"names/{fn}.yml"])
-        # run(["git", "commit", "-m", f"Add {domain}"])
-        # run(["git", "push", "-u", "origin", domain])
-        # run(["gh", "pr", "create", "-t", f"Add {domain}", "-b", f"Hey @{handle}, would you like to merge this PR adding you to https://namehack.club?"])
-        # run(["git", "checkout", "main"])
+            # run(["git", "checkout", "-b", domain])
+            # run(["git", "add", f"names/{fn}.yml"])
+            # run(["git", "commit", "-m", f"Add {domain}"])
+            # run(["git", "push", "-u", "origin", domain])
+            # run(["gh", "pr", "create", "-t", f"Add {domain}", "-b", f"Hey @{handle}, would you like to merge this PR adding you to https://namehack.club?"])
+            # run(["git", "checkout", "main"])
