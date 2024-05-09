39335ece8f46b5a357a0b3c404de8d51fb45b915
Yuval Adam <_@yuv.al>
2024-05-09 15:19:24 +0200

    Attempt netlify build workaround

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,8 +1,15 @@
+import os
+
 from .base import cli
 
 from .build import build
-from .clean import clean
-from .ghpr import ghpr
-from .scan import scan
 
-__all__ = [cli, build, clean, ghpr, scan]
+__all__ = [cli, build]
+
+# ugly workaround to avoid build failures in Python 3.8
+if "NETLIFY" not in os.environ:
+    from .clean import clean
+    from .ghpr import ghpr
+    from .scan import scan
+
+    __all__ += [clean, ghpr, scan]
