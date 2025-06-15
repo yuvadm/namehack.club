600f54c92b4a9173246e37ccbe13c6aaff4992ca
Yuval Adam <_@yuv.al>
2025-06-15 14:26:59 +0200

    Use proper astro url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://namehack.club',
+  
   vite: {
     plugins: [tailwindcss()]
   },
