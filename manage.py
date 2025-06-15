2e0755cc68e14bda95abe9472754ae5f4e760a04
Yuval Adam <_@yuv.al>
2025-06-15 13:44:16 +0200

    Remove all old files before migration

--- manage.py
+++
@@ -1,9 +0,0 @@
-import click
-
-from cli import cli
-
-cli = click.CommandCollection(sources=[cli])
-
-# this file is only required for Netlify build to be able to trigger CLI
-if __name__ == "__main__":
-    cli()
